5cf324eb0753033e78d1be78a413ded7b3e7c662
Bruno <brunoviniciusfroes@gmail.com>
2024-05-31 17:57:19 -0300

    Desafio concluido com Sucesso!
    Signed-off-by: Bruno <brunoviniciusfroes@gmail.com>

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+// -User: REALIZADO!
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("123456", "Modelo 1", "1111111111", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Facebook");
+
+Console.WriteLine("\nSmartphone iPhone:");
+Smartphone iphone = new Iphone("4987", "Modelo 2", "22222222222", 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Whatsapp");
